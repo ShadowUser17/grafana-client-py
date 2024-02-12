6563073dbde34cc2dc93b22f1231abca0e856258
ShadowUser17 <linuxforce17@gmail.com>
2024-02-12 21:32:07 +0200

    Add backup.py (empty)

---
+++ backup.py
@@ -0,0 +1,16 @@
+import grafana
+import pathlib
+
+
+class Backup:
+    def __init__(self, client: grafana.Grafana, base_dir: str) -> None:
+        pass
+
+    def backup_folders(self) -> None:
+        pass
+
+    def backup_dashboards(self) -> None:
+        pass
+
+    def backup_batasources(self) -> None:
+        pass
