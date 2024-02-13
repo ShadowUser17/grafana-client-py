27d1392532240508370e83e929367f9f3acf017b
ShadowUser17 <linuxforce17@gmail.com>
2024-02-13 15:42:33 +0200

    Implemented Backup.backup_folders function.

--- backup.py
+++ backup.py
@@ -1,13 +1,39 @@
-import grafana
+import json
 import pathlib
+import grafana
+import traceback
 
 
 class Backup:
     def __init__(self, client: grafana.Grafana, base_dir: str) -> None:
-        pass
+        self._grafana = client
+        self._base_path = pathlib.Path(base_dir)
+        self._base_path.mkdir(parents=True, exist_ok=True)
+
+        self._folder_data = "data.json"
+        self._folder_access = "access.json"
 
     def backup_folders(self) -> None:
-        pass
+        for folder_item in self._grafana.list_folders():
+            try:
+                folder_path = self._base_path.joinpath(str(folder_item["id"]))
+                folder_path.mkdir(exist_ok=True)
+                print("Create folder directory:", folder_path)
+
+                # Store folder object in data.json
+                tmp = json.dumps(self._grafana.get_folder_by_uid(folder_item["uid"]))
+                folder_data = folder_path.joinpath(self._folder_data)
+                folder_data.write_text(tmp)
+                print("Store folder data:", folder_data)
+
+                # Store folder permissions to access.json
+                tmp = json.dumps(self._grafana.get_folder_permissions(folder_item["uid"]))
+                folder_access = folder_path.joinpath(self._folder_access)
+                folder_access.write_text(tmp)
+                print("Store folder access:", folder_access)
+
+            except Exception:
+                traceback.print_exc()
 
     def backup_dashboards(self) -> None:
         pass
